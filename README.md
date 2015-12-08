c6dafede0735e0489d494ca4099eaa2d5df30d35
Matthew <student@pyclassvm>
2015-12-07 23:26:15 -0500

    Adding our website project to git

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# Website Project Example
+## Maryland Election Administration Project
+
+## Synopsis of project
 
 This website is part of a Johns Hopkins class that uses Python to collect and analyze data. The website showcases all the skills learned in the class.
+
+In my project, I used Election Administration bulk data in CSV format. I cleaned the data where possible when I found obviously incorrect data. In some cases, I was unable to do so and I will note where I do not believe the data are accurate in my results. I created metrics for voters in a single state to compare how their election officials (in this case, county-level officials) are conducting elections. I chose Maryland because it is where I currently live and because the data were some of the most complete statewide.
+
+## Data
+
+Election administration data from the bienniel Election Administration and Voting Surveys in 2012 and 2014.
+
+## Contributors
+
+Matthew Weil
+
+## License
+
+MIT License (MIT)
--- app.py
+++ app.py
@@ -29,6 +29,102 @@ def question():
 def description():
 	return render_template('description.html')
 
+@app.route('/allegany')
+def allegany():
+	return render_template('allegany.html')
+
+@app.route('/anne_arundel')
+def anne_arundel():
+	return render_template('anne_arundel.html')
+
+@app.route('/baltimore_city')
+def baltimore_city():
+	return render_template('baltimore_city.html')
+
+@app.route('/baltimore_county')
+def baltimore_county():
+	return render_template('baltimore_county.html')
+
+@app.route('/calvert')
+def calvert():
+	return render_template('calvert.html')
+
+@app.route('/caroline')
+def caroline():
+	return render_template('caroline.html')
+
+@app.route('/carroll')
+def carroll():
+	return render_template('carroll.html')
+
+@app.route('/cecil')
+def cecil():
+	return render_template('cecil.html')
+
+@app.route('/charles')
+def charles():
+	return render_template('charles.html')
+
+@app.route('/dorchester')
+def dorchester():
+	return render_template('dorchester.html')
+
+@app.route('/frederick')
+def frederick():
+	return render_template('frederick.html')
+
+@app.route('/garrett')
+def garrett():
+	return render_template('garrett.html')
+
+@app.route('/harford')
+def harford():
+	return render_template('harford.html')
+
+@app.route('/howard')
+def howard():
+	return render_template('howard.html')
+
+@app.route('/kent')
+def kent():
+	return render_template('kent.html')
+
+@app.route('/montgomery')
+def montgomery():
+	return render_template('montgomery.html')
+
+@app.route('/prince_georges')
+def prince_georges():
+	return render_template('prince_georges.html')
+
+@app.route('/queen_annes')
+def queen_annes():
+	return render_template('queen_annes.html')
+
+@app.route('/somerset')
+def somerset():
+	return render_template('somerset.html')
+
+@app.route('/st_marys')
+def st_marys():
+	return render_template('st_marys.html')
+
+@app.route('/talbot')
+def talbot():
+	return render_template('talbot.html')
+
+@app.route('/washington')
+def washington():
+	return render_template('washington.html')
+
+@app.route('/wicomico')
+def wicomico():
+	return render_template('wicomico.html')
+
+@app.route('/worcester')
+def worcester():
+	return render_template('worcester.html')
+
 if __name__ == "__main__":
 	app.run(debug=True)
 
